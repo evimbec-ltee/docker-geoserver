f4221d25e6840374a5a7f23d3af79aacc99740a8
SLetourneau <sylvain.letourneau@magika.ca>
2023-02-19 16:51:08 -0500

    Fix the ECR docker push instructions.  Disabled auto downloading of unused extensions.

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ ECR_REGISTRY=014233203916.dkr.ecr.ca-central-1.amazonaws.com
 ECR_REPOSITORY=internal-images
 
 docker-compose -f docker-compose-build.yml build
-source .env && docker tag kartoza/geoserver-mod:$GS_VERSION $ECR_REGISTRY/$ECR_REPOSITORY:kartoza/geoserver-mod:$GS_VERSION
-source .env && docker push $ECR_REGISTRY/$ECR_REPOSITORY:kartoza/geoserver-mod:$GS_VERSION
+
+aws ecr get-login-password --region ca-central-1 | docker login --username AWS --password-stdin $ECR_REGISTRY/$ECR_REPOSITORY
+source .env && docker tag kartoza/geoserver-mod:$GS_VERSION $ECR_REGISTRY/$ECR_REPOSITORY:kartoza-geoserver-mod_$GS_VERSION
+source .env && docker push $ECR_REGISTRY/$ECR_REPOSITORY:kartoza-geoserver-mod_$GS_VERSION
 
 
